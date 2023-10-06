c8918b2ac9d52b7b010813b324d33e74e4df7228
nilswendeh <nilsheinrich91@gmail.com>
2023-10-06 16:24:59 +0200

    commit for the day

--- exemplary_use_of_visualize_sequence.py
+++ exemplary_use_of_visualize_sequence.py
@@ -3,12 +3,12 @@ from visualize_gaming_sequence import visualize_sequence
 
 # data parameters (to identify input_ and eye_data files)
 
-id_code = 'EU29TT1'  # 'MO07LN1' #'AE07EM1'
-done_ = 'done'
-n_run = '46'  # 5
-arg_comb = '6FW'  # '5TW'
-start_time = 35
-end_time = 40
+id_code = 'AR02AA'  #'AR02AA' #'EU29TT1' #'AE07EM1'
+done_ = 'done'  #'done'
+n_run = '22'  # '46'
+arg_comb = '5TS'  # '6FW'
+start_time = 3.478442
+end_time = 16.478442
 
 # call visualize function
 visualize_sequence(id_code=id_code, done_=done_, n_run=n_run, arg_comb=arg_comb, start_time=start_time, end_time=end_time, safe_ani=False)
