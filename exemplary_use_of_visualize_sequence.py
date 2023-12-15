1c11131706813166a2a22373daa36acbecc62e8c
nilswendeh <nilsheinrich91@gmail.com>
2023-12-15 18:19:39 +0100

    ffmpeg works for plt animation

--- exemplary_use_of_visualize_sequence.py
+++ exemplary_use_of_visualize_sequence.py
@@ -3,13 +3,13 @@ from visualize_gaming_sequence import visualize_sequence
 
 # data parameters (to identify input_ and eye_data files)
 
-id_code = 'AR02AA'  #'AR02AA' #'EU29TT1' #'AE07EM1'
-done_ = 'done'  #'done'
-n_run = '22'  # '46'
-arg_comb = '5TS'  # '6FW'
-start_time = 3.478442
-end_time = 16.478442
+id_code = 'UD06AD'
+done_ = 'crashed'  #'done'
+n_run = '30'  #'34'
+arg_comb = '6TW'
+start_time = 4.5
+end_time = 14.5
 
 # call visualize function
-visualize_sequence(id_code=id_code, done_=done_, n_run=n_run, arg_comb=arg_comb, start_time=start_time, end_time=end_time, safe_ani=False)
+visualize_sequence(id_code=id_code, done_=done_, n_run=n_run, arg_comb=arg_comb, start_time=start_time, end_time=end_time, safe_ani=True)
 # visualize_sequence(id_code=id_code, done_=done_, n_run=n_run, arg_comb=arg_comb, safe_ani=True)
